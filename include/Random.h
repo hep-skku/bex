4fdc2470537f95695e4af548f4a2036c50bf401a
Junghwan John Goh <jhgoh@cern.ch>
2013-07-19 04:02:48 +0900

    bugfix, more random functions, pickers from list

--- include/Random.h
+++ include/Random.h
@@ -10,10 +10,24 @@ public:
   double uniform(const double min, const double max);
   // Linear ramp distribution
   double ramp(const double min, const double max);
+  // Pick an integer from range [min, max]
+  int pick(const int min, const int max);
+  // Pick one from list
+  template<typename IteratorType>
+  IteratorType pick(IteratorType begin, IteratorType end)
+  {
+    const size_t shift(rand()*(end-begin));
+    return begin+shift;
+  }
 
 private:
   boost::mt19937 rnd_;
+  double rnd_min_, rnd_width_;
 
+  inline double rand()
+  {
+    return (rnd_()-rnd_min_)/rnd_width_;
+  }
   double shift(const double x, const double min, const double max) const;
 };
 
--- src/Random.cc
+++ src/Random.cc
@@ -6,20 +6,27 @@ using namespace std;
 Random::Random(const unsigned int seed)
 {
   rnd_.seed(seed);
+  rnd_min_ = rnd_.min();
+  rnd_width_ = rnd_.max()-rnd_min_;
 }
 
 double Random::shift(const double x, const double min, const double max) const
 {
-  return (x-min)/(max-min) + min;
+  return x*(max-min) + min;
 }
 
 double Random::uniform(const double min, const double max)
 {
-  return shift(rnd_(), min, max);
+  return shift(rand(), min, max);
 }
 
 double Random::ramp(const double min, const double max)
 {
-  return shift(sqrt(rnd_()), min, max);
+  return shift(sqrt(rand()), min, max);
+}
+
+int Random::pick(const int min, const int max)
+{
+  return int(rand()*(max+1-min));
 }
 
