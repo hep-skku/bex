5679e8bb0798e13b200ce7dbeecd8a50219e6bd9
Junghwan John Goh <jhgoh@cern.ch>
2013-07-19 05:12:10 +0900

    added BH models

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
 #include "include/ConfigReader.h"
+#include "include/ADDModel.h"
+#include "include/RSModel.h"
 
 #include <iostream>
 #include <vector>
@@ -10,6 +12,11 @@ int main(int argc, char* argv[])
 {
   ConfigReader cfg(argv[1], argc-2, argv+2);
   cfg.print();
+
+  AbsModel* model = 0;
+  const string modelName = cfg.get<string>("model");
+  if ( modelName == "ADD" ) model = new ADDModel(cfg);
+  else if ( modelName == "RS" ) model = new RSModel(cfg);
 
   return 0;
 }
