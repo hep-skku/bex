f6e83740c3e676450d85a3218e8411d97e486135
Junghwan John Goh <jhgoh@cern.ch>
2013-07-19 07:35:52 +0900

    common variables

--- include/AbsModel.h
+++ include/AbsModel.h
@@ -2,11 +2,22 @@
 #define AbsModel_H
 
 #include "include/ConfigReader.h"
+#include "include/PDFInterface.h"
+#include "include/Random.h"
 
 class AbsModel
 {
 public:
   AbsModel(const ConfigReader& cfg);
+  virtual ~AbsModel();
+
+protected:
+  Random* rnd_;
+  PDFInterface* pdf_;
+
+  double beamEnergy_;
+  double massMin_, massMax_;
+  double mD_;
 };
 
 #endif
--- src/AbsModel.cc
+++ src/AbsModel.cc
@@ -2,4 +2,17 @@
 
 AbsModel::AbsModel(const ConfigReader& cfg)
 {
+  beamEnergy_ = cfg.get<double>("beamEnergy");
+  massMin_ = cfg.get<double>("massMin");
+  massMax_ = cfg.get<double>("massMax");
+  mD_   = cfg.get<double>("mD");
+
+  rnd_ = new Random(cfg.get<int>("seed"));
+  pdf_ = new PDFInterface(cfg.get<int>("pdfId"));
+}
+
+AbsModel::~AbsModel()
+{
+  delete rnd_;
+  delete pdf_;
 }
