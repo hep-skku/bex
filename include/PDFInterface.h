5c789230b67a87d86ebead61e5b65527d2455f3a
Junghwan John Goh <jhgoh@cern.ch>
2013-07-20 16:21:20 +0900

    PDF container datatype

--- include/PDFInterface.h
+++ include/PDFInterface.h
@@ -3,14 +3,33 @@
 
 #include <vector>
 
+class PDFInterface;
+
+class PDF
+{
+public:
+  PDF();
+  double operator()(const int pdgId) const;
+  double getSumPDF() const { return sumPDF_; }
+  const static int nParton = 13;
+
+private:
+  void update(); // Re-calculate cached values
+
+  double pdfValues_[nParton];
+  double sumPDF_;
+
+  friend class PDFInterface;
+};
+
 class PDFInterface
 {
 public:
-  PDFInterface(const int pdfId);
-  std::vector<double> loadPDF(const double x, const double q) const;
+  PDFInterface(const int pdfSet);
+  void loadPDF(const double x, const double q, PDF& pdf) const;
 
 private:
-  int pdfId_;
+  int pdfSet_;
 
 };
 
--- src/PDFInterface.cc
+++ src/PDFInterface.cc
@@ -1,14 +1,54 @@
 #include "include/PDFInterface.h"
 #include "LHAPDF/LHAPDF.h"
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
-PDFInterface::PDFInterface(const int pdfId)
+PDFInterface::PDFInterface(const int pdfSet)
 {
-  pdfId_ = pdfId;
-  LHAPDF::initPDF(pdfId_);
+  pdfSet_ = pdfSet;
+  LHAPDF::initPDFSet(pdfSet, 0);
 }
 
-std::vector<double> PDFInterface::loadPDF(const double x, const double q) const
+void PDFInterface::loadPDF(const double x, const double q, PDF& pdf) const
 {
-  return LHAPDF::xfx(x, q);
+  LHAPDF::xfx(x, q, pdf.pdfValues_);
+  for ( int i=0; i<PDF::nParton; ++i )
+  {
+    pdf.pdfValues_[i] /= x; 
+  }
+  pdf.update();
+}
+
+PDF::PDF()
+{
+  for ( int i=0; i<nParton; ++i )
+  {
+    pdfValues_[i] = 0;
+  }
+}
+
+void PDF::update()
+{
+  sumPDF_ = 0;
+  for ( int i=0; i<nParton; ++i )
+  {
+    if ( pdfValues_[i] < 0 ) pdfValues_[i] = 0;
+    sumPDF_ += pdfValues_[i];
+  }
+}
+
+double PDF::operator()(const int pdgId) const
+{
+  // Comment from LHAPDF/LHAPDF.h
+  /// Nucleon PDF: fills primitive 13 element array pointed at by @a results with
+  /// \f$ x f(x, Q) \f$ with index \f$ 0 < i < 12 \f$.
+  /// @arg 0..5 = \f$ \bar{t} \f$, ..., \f$ \bar{u} \f$, \f$ \bar{d} \f$;
+  /// @arg 6 = \f$ g \f$;
+  /// @arg 7..12 = \f$ d \f$, \f$ u \f$, ..., \f$ t \f$.
+  if ( pdgId == 21 ) return pdfValues_[6];
+  if ( std::abs(pdgId) == 0 or std::abs(pdgId) > 6 ) return 0; // FIXME : raise exception?
+
+  return pdfValues_[pdgId+6];
 }
 
