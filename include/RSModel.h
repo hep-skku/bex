4e7b7368b46d110735e290e26b38eba949685098
Junghwan John Goh <jhgoh@cern.ch>
2013-07-20 16:23:10 +0900

    implemented RS model xsec calculation

--- include/RSModel.h
+++ include/RSModel.h
@@ -7,7 +7,13 @@ class RSModel : public AbsModel
 {
 public:
   RSModel(const ConfigReader& cfg);
-  virtual double calculatePartonCrossSection();
+  virtual double calculatePartonWeight(const double m, const PDF& pdf1, const PDF& pdf2);
+
+private:
+  double kn_, kn2_;
+
+  std::vector<double> prodWeights_;
+  double rs_wGG_, rs_wBG_, rs_wBB_;
 };
 
 #endif
--- src/RSModel.cc
+++ src/RSModel.cc
@@ -1,12 +1,44 @@
 #include "include/RSModel.h"
+#include <iostream>
+using namespace std;
 
 RSModel::RSModel(const ConfigReader& cfg):
   AbsModel(cfg)
 {
+  kn2_ = 2./3*pi_;
+  formFactor_ = kn2_*pi_;
+  if ( formFactorName_ == "YOSHINO" )
+  {
+    formFactor_ = 1; // FIXME: Implement  Yoshino-Rychkov
+  }
+  else if ( formFactorName_ == "FIOP" )
+  {
+    const double fiop = 16./13;
+    formFactor_ *= fiop;
+  }
+
+  prodWeights_ = cfg.get<std::vector<double> >("prodWeights");
+  // wGG = C_gg
+  rs_wGG_ = prodWeights_[0];
+  // wBG = C_gQ + C_gd
+  rs_wBG_ = prodWeights_[2] + prodWeights_[8];
+  // wBB = C_QQ + C_Qd + C_dd
+  rs_wBB_ = prodWeights_[1] + prodWeights_[7] + prodWeights_[6];
 }
 
-double RSModel::calculatePartonCrossSection()
+double RSModel::calculatePartonWeight(const double m, const PDF& pdf1, const PDF& pdf2)
 {
-  return 0;
+  const double rssqr = m/mD_/mD_/mD_; // We will do 5D only
+  const double u = m*m/s_;
+  const double weightParton = rssqr*(1./massMax_-1./massMin_)*m*u*log(u);
+
+  // Consider suppression factor due to profile in extra dimension
+  // Assume partons are not polarized; 50% left and 50% right handed
+  double weightProfile = 0;
+  weightProfile += rs_wGG_*pdf1(0)*pdf2(0); // Gluon+Gluon
+  weightProfile += rs_wBG_*(pdf1(0)*pdf2(5) + pdf1(5)*pdf2(0)); // Gluon+Bottom
+  weightProfile += rs_wBB_*pdf1(5)*pdf2(5); // Bottom+Bottom
+
+  return weightParton*weightProfile;
 }
 
