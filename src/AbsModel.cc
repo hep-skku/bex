2cbb4a303d09195e2d89af3616ad010731e85d05
Junghwan John Goh <jhgoh@cern.ch>
2013-07-22 18:10:14 +0900

    ranged getters, read mLoss table, nDim as member variable

--- src/AbsModel.cc
+++ src/AbsModel.cc
@@ -1,17 +1,41 @@
 #include "include/AbsModel.h"
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
 
 using namespace std;
+
+istream& operator>>(istream& in, std::vector<std::pair<double, double> >& data)
+{
+  string line;
+  while ( getline(in, line) )
+  {
+    const size_t commentPos = line.find('#');
+    if ( commentPos != string::npos ) line.erase(commentPos);
+    boost::algorithm::trim(line);
+    if ( line.empty() or line[0] == '#' ) continue;
+    std::replace(line.begin(), line.end(), ',', ' ');
+
+    stringstream ss(line);
+    double x, y;
+    ss >> x >> y;
+    data.push_back(make_pair(x, y));
+  }
+  return in;
+}
 
 AbsModel::AbsModel(const ConfigReader& cfg)
 {
   isValid_ = false;
 
-  beamEnergy_ = cfg.get<double>("beamEnergy");
-  massMin_ = cfg.get<double>("massMin");
-  massMax_ = cfg.get<double>("massMax");
-  mD_   = cfg.get<double>("mD");
+  beamEnergy_ = cfg.get<double>("beamEnergy", 0, 1e9);
+  massMin_ = cfg.get<double>("massMin", 0., beamEnergy_);
+  massMax_ = cfg.get<double>("massMax", massMin_, beamEnergy_);
+  massMin_ = cfg.get<double>("massMin", 0., massMax_); // Check once again to check reversed input
+  mD_   = cfg.get<double>("mD", 0., 1e9);
+  nDim_ = cfg.get<int>("dimension", 4, 11);
 
   ConfigReader::MenuType formFactorMenu;
   formFactorMenu["Yoshino"] = FormFactorType::YOSHINO;
@@ -29,6 +53,19 @@ AbsModel::AbsModel(const ConfigReader& cfg)
     mLossMenu["Const"] = MassLossType::CONST;
     mLossMenu["Fixed"] = MassLossType::CONST;
     mLossType_ = cfg.get("mLossType", mLossMenu);
+  }
+  // Build Impact parameter vs mass loss factor tables
+  if ( mLossType_ == MassLossType::CONST )
+  {
+    const double mLossFactor = cfg.get<double>("mLossFactor", 0, 1);
+    mLossTab_.push_back(std::make_pair(0., mLossFactor));
+    mLossTab_.push_back(std::make_pair(1., mLossFactor));
+  }
+  else if ( mLossType_ == MassLossType::YOSHINO )
+  {
+    const std::string mLossFileName = (boost::format("data/yoshino/MLB_N%1%.data") % (nDim_-4)).str();
+    ifstream mLossFile(mLossFileName.c_str());
+    mLossFile >> mLossTab_;
   }
 
   rnd_ = new Random(cfg.get<int>("seed"));
