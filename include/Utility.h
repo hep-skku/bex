7c5d50b0ee61bc54621f2963450b087b0ac81b02
Junghwan John Goh <jhgoh@cern.ch>
2013-07-22 20:38:44 +0900

    removed namespace, added paired data reader interface

--- include/Utility.h
+++ include/Utility.h
@@ -1,10 +1,11 @@
 #ifndef Utility_H
 #define Utility_H
 
-namespace Utility
-{
-  void printCrossSection(const double xsec, const double xsecErr);
-}
+#include <iostream>
+#include <vector>
+
+void printCrossSection(const double xsec, const double xsecErr);
+std::istream& operator>>(std::istream& in, std::vector<std::pair<double, double> >& data);
 
 #endif
 
