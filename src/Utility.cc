c35570404fd3318f1fd6ad0ed23a0053af893fd4
Junghwan John Goh <jhgoh@cern.ch>
2013-07-22 20:39:16 +0900

    removed namespace, added paired data reader interface

--- src/Utility.cc
+++ src/Utility.cc
@@ -1,10 +1,33 @@
 #include "include/Utility.h"
 #include <iostream>
+#include <sstream>
 
-using namespace Utility;
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+
 using namespace std;
 
-void Utility::printCrossSection(const double xsec, const double xsecErr)
+void printCrossSection(const double xsec, const double xsecErr)
 {
   cout << "Cross section = " << xsec << " +- " << xsecErr << " (pb)\n";
 }
+
+istream& operator>>(istream& in, std::vector<std::pair<double, double> >& data)
+{
+  string line;
+  while ( getline(in, line) )
+  {
+    const size_t commentPos = line.find('#');
+    if ( commentPos != string::npos ) line.erase(commentPos);
+    boost::algorithm::trim(line);
+    if ( line.empty() or line[0] == '#' ) continue;
+    std::replace(line.begin(), line.end(), ',', ' ');
+
+    stringstream ss(line);
+    double x, y;
+    ss >> x >> y;
+    data.push_back(make_pair(x, y));
+  }
+  return in;
+}
+
