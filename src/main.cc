435c9d4f2a75d8852b5e5c4f1e3ffd5d546de1a3
Junghwan John Goh <jhgoh@cern.ch>
2013-07-24 00:57:44 +0900

    restricted event number not to diverge output

--- src/main.cc
+++ src/main.cc
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
   ConfigReader cfg(argv[1], argc-2, argv+2);
   cfg.print();
 
-  const int nEvent = cfg.get<int>("nEvent");
+  const int nEvent = cfg.get<int>("nEvent", 0, 100000);
 
   AbsModel* model = 0;
   const string modelName = cfg.get<string>("model");
