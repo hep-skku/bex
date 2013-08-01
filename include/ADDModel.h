8694181854652c765eec99f46bf15739ab9b2e85
Junghwan John Goh <jhgoh@cern.ch>
2013-08-02 01:41:03 +0900

    implemented already in AbsModel

--- include/ADDModel.h
+++ include/ADDModel.h
@@ -8,9 +8,6 @@ class ADDModel : public AbsModel
 public:
   ADDModel(const ConfigReader& cfg);
   virtual double calculatePartonWeight(const double m, const PDF& pdf1, const PDF& pdf2);
-  virtual bool selectDecay(const NVector& bh_momentum, const NVector& bh_position,
-                           const int bh_charge, const double bh_spin,
-                           Particle& daughter);
 
 private:
 
