e308c88fb854cf04a8e86ae7ed484bcafe8ba3ac
Junghwan John Goh <jhgoh@cern.ch>
2014-03-07 13:50:15 +0900

    use std::vector by default, move implementation to cc file

--- include/Random.h
+++ include/Random.h
@@ -26,49 +26,8 @@ public:
     const size_t shift(rand()*(end-begin));
     return begin+shift;
   }
-  template<typename VectorType>
-  int pickFromCDF(const VectorType& v)
-  {
-    // Check validity of CDF : is it monolothic array?
-    for ( int i=0, n=v.size()-1; i<n; ++i )
-    {
-      if ( v[i] > v[i+1] )
-      {
-        std::cerr << "Invalid CDF, array is not monolothic\n";
-        return -1;
-      }
-    }
-
-    const double x = uniform(0, v.back());
-
-    // Do binary search
-    unsigned int lo = 0, hi=v.size()-1;
-    // Special case when hitting upper bound, x == v[hi]
-    // This case can appear depending on implementation of generator
-    if ( x == v[hi] ) return hi;
-    while ( true )
-    {
-      const unsigned int curr = (hi+lo)/2;
-      const double currX = v[curr];
-      if ( x < currX ) hi = curr;
-      else if ( currX <= x ) lo = curr;
-      if ( hi - lo <= 1 ) break;
-    }
-
-    return lo;
-  }
-  template<typename VectorType>
-  int pickFromHist(const VectorType& v)
-  {
-    VectorType cdf(v.size()+1);
-    cdf[0] = 0;
-    // Make it CDF
-    for ( int i=0, n=v.size(); i<n; ++i )
-    {
-      cdf[i+1] = cdf[i]+v[i];
-    }
-    return pickFromCDF(cdf);
-  }
+  int pickFromCDF(const std::vector<double>& v);
+  int pickFromHist(const std::vector<double>& v);
 
 private:
   boost::mt19937 rnd_;
--- src/Random.cc
+++ src/Random.cc
@@ -47,3 +47,46 @@ int Random::pick(const int min, const int max)
   return int(rand()*(max+1-min));
 }
 
+int Random::pickFromCDF(const std::vector<double>& v)
+{
+  // Check validity of CDF : is it monolothic array?
+  for ( int i=0, n=v.size()-1; i<n; ++i )
+  {
+    if ( v[i] > v[i+1] )
+    {
+      std::cerr << "Invalid CDF, array is not monolothic\n";
+      return -1;
+    }
+  }
+
+  const double x = uniform(0, v.back());
+
+  // Do binary search
+  unsigned int lo = 0, hi=v.size()-1;
+  // Special case when hitting upper bound, x == v[hi]
+  // This case can appear depending on implementation of generator
+  if ( x == v[hi] ) return hi;
+  while ( true )
+  {
+    const unsigned int curr = (hi+lo)/2;
+    const double currX = v[curr];
+    if ( x < currX ) hi = curr;
+    else if ( currX <= x ) lo = curr;
+    if ( hi - lo <= 1 ) break;
+  }
+
+  return lo;
+}
+
+int Random::pickFromHist(const std::vector<double>& v)
+{
+  std::vector<double> cdf(v.size()+1);
+  cdf[0] = 0;
+  // Make it CDF
+  for ( int i=0, n=v.size(); i<n; ++i )
+  {
+    cdf[i+1] = cdf[i]+v[i];
+  }
+  return pickFromCDF(cdf);
+}
+
